0099f0a2719291857639491c0a5c46863814614f
EmonMajumder <43067648+EmonMajumder@users.noreply.github.com>
2020-10-16 14:33:41 -0300

    Initial commit

---
+++ src/main/java/com/example/REDspace_StarWar_API/ReDspaceStarWarApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.REDspace_StarWar_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReDspaceStarWarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReDspaceStarWarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/REDspace_StarWar_API/ReDspaceStarWarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.REDspace_StarWar_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReDspaceStarWarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
